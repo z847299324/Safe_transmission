f457fdb12b7035ed23a39373c57dbf81ece2b648
123 <321>
2018-07-26 18:37:40 +0800

    共享内存存指定对象封装

--- Safe_transmission/shmclass/test.cpp
+++ Safe_transmission/shmclass/test.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 #include "shmClass.h"
 using namespace std;
+#include"shmkey.h"
+#include<string.h>
+#include<stdlib.h>
+using namespace std;
 int main()
 {
-    shmClass shm(1,1024);
-    void* head = shm.getshm();
-    shm.unshm();
-    shm.deshm();
+    keyinfo *info=new keyinfo;
+    strcpy(info->clientId,"23451");
+    info->status = 100;
+    strcpy(info->serverId,"23451");
+    info->seckeyid = 10010;
+    strcpy(info->seckey,"23451");
 
-    cout << "Hello world" << endl;
+    cout << info->clientId<<info->status << endl;
+    
+    //共享内存操作测试
+    shmkey shmk(100,100);
+    //存
+    shmk.shmPutdata(info);
+
+    keyinfo * datta = NULL;
+    //取
+    datta = shmk.shmGetdata(info->clientId,info->serverId,info->seckeyid);
+    
+    cout<<datta->clientId<<endl;
     return 0;
 }
 
